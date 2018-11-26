951c11ce043bd300b93fd3b2afe42a471ed0bb75
daniriian <daniriian@gmail.com>
2018-11-26 21:42:07 +0200

    final adjustements

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-let nav = document.getElementById("topNavBar");
+// Changing the defaults
+window.sr = ScrollReveal({ reset: true });
 
-window.addEventListener('scroll', () => {
-    if (pageYOffset - 4 * nav.clientHeight > 0) {
-        if (nav.classList.contains('navbar-primary')) {
-            nav.classList.toggle('navbar-primary');
-        }
-    }
+// Customizing a reveal set
+sr.reveal('header', { opacity: 0.2, duration: 1000 });
 
-    else if (pageYOffset - 4 * nav.clientHeight < 0) {
-        if (!nav.classList.contains('navbar-primary')) {
-            nav.classList.toggle('navbar-primary');
-        }
-    }
-})
+sr.reveal('.section-hero', { opacity: 0.3, duration: 1000, scale: 0.8 });
+
+sr.reveal('.hero-col', { opacity: 0.3, duration: 1000, delay: 500, scale: 0.5 });
+
+sr.reveal('.grow-together', { opacity: 0.3, duration: 1000, delay: 500 });
+
+sr.reveal('.flowing-conversations', { opacity: 0.3, duration: 1000, delay: 500, scale: 0.5 });
+
+sr.reveal('.users', { opacity: 0.3, duration: 1000, delay: 500 });
+
+sr.reveal('.get-started', { opacity: 0.3, duration: 1000, delay: 500, scale: 0.5 });
+
+sr.reveal('.footer', { opacity: 0.3, duration: 1000, delay: 100 });
+
+sr.reveal('.newsletter', { opacity: 0, duration: 1000, delay: 300, scale: 0.5 });
+
+sr.reveal('.contact-form', { opacity: 0, duration: 1000, delay: 300, scale: 0.5 });
